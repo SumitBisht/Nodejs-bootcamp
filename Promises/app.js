6386794bd651ea61bba6dd1f5ba7dd39a6bd5a28
Sumit Bisht <sumitbisht1987@gmail.com>
2014-07-21 00:01:03 +0530

    updated example

--- Promises/app.js
+++ Promises/app.js
@@ -6,25 +6,39 @@ var app = express();
 
 var readFile = q.nbind(fs.readFile);
 
-
-var p1 = readFile('one.txt').then(function(val) {
-  console.log('file 1', val);
-  return val; 
+app.get('/callback', function(req, res){
+	fs.readFile('./one.txt', 'utf-8', function(err, data){
+		if(err){
+			console.error(err);
+			return;
+		}
+		res.send(data);
+	});
 });
 
-var p2 = readFile('two.txt').then(function(val) {
-  console.log('file 2', val);
-});
 
-app.get('/promise', function(req, res){
-  q.all([p1,p2]).then(function() {
-    console.log('read both files!', JSON.stringify(p1), JSON.stringify(p2));
-  }, function(err) {
-    console.log('something bad happened', err.message ? err.message : '');
-    if (err.stack) {
-      console.log(err.stack);
-    }
+app.get('/*', function(req, res){
+	// Use q.all to wait on both to be finished.
+	// if things to be used are executed in parallel before, the overall
+	// response time will get reduced.
+	q.all([p1,p2]).then(function() {
+	  console.log('read both files!');
+	}, function(err) {
+	  console.log('something bad happened', err.message ? err.message : '');
+	  if (err.stack) {
+	    console.log(err.stack);
+	  }
+    });
+
   });
-});
 
 var server = app.listen(8080, function() {});
+
+var p1 = readFile('one.txt', 'utf-8').then(function(val) {
+
+  console.log('file 1', 'utf-8', val); 
+});
+
+var p2 = readFile('two.txt', 'utf-8').then(function(val) {
+  console.log('file 2', 'utf-8', val);
+});
