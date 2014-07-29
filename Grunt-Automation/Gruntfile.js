358a4f399c226f5839cfe6ef79fbdbaaa24a969d
sumit <sumitbisht1987@gmail.com>
2014-07-29 17:41:11 +0530

    added minify task with a sample example

--- Grunt-Automation/Gruntfile.js
+++ Grunt-Automation/Gruntfile.js
@@ -1,10 +1,24 @@
 module.exports = function(grunt){
   
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+
+    uglify: {
+      options: {
+        banner: '/**\n *  Package: <%= pkg.name %>\n *  Version: <%= pkg.version %>\n'+
+        ' * Generated: <%= grunt.template.today("dd-mm-yyyy") %>\n**/\n'
+      },
+      my_target: {
+        files:{
+          'js/generated_min.js': ['js/hello.js']
+        }
+      }
+    }
   });
+
 
   grunt.registerTask('default', function(){
     grunt.log.writeln('Hello from grunt\'s default task');
   });
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 }
---
+++ Grunt-Automation/js/generated_min.js
@@ -0,0 +1,6 @@
+/**
+ *  Package: grunt-automator
+ *  Version: 1.0.0
+ * Generated: 29-07-2014
+**/
+window.onload=function(){console.log("This is the page load"),document.getElementById("a_div").innerText="This content comes within the div"};
